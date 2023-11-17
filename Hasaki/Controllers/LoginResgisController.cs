70e0906a32e1fe2eb065ff49e87d0a75d91d118d
Atula0 <lomki12231@gmail.com>
2023-11-17 21:08:36 +0700

    Update đăng ký đăng nhập, qên tài khoản

--- Hasaki/Controllers/LoginResgisController.cs
+++ Hasaki/Controllers/LoginResgisController.cs
@@ -10,6 +10,8 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity.Migrations;
+using System.Web.UI.HtmlControls;
 
 namespace Hasaki.Controllers
 {
@@ -105,6 +107,17 @@ namespace Hasaki.Controllers
                         db.KhachHangs.Add(khachhang);
                         db.SaveChanges();
                         // Gửi email đăng ký thành công
+                        SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                        client.EnableSsl = true;
+                        client.UseDefaultCredentials = false;
+                        client.Credentials = new NetworkCredential("lomki12231@gmail.com", "poka gibp icls gkvh");
+
+                        MailMessage mailMessage = new MailMessage();
+                        mailMessage.From = new MailAddress("lomki12231@gmail.com");
+                        mailMessage.To.Add(email);
+                        mailMessage.Body = $"Đăng ký thành công \n Tài khoản: {email} \n Mật khẩu: {matkhau1}";
+                        mailMessage.Subject = "Hasaki xin chào khách hàng mới !";
+                        client.Send(mailMessage);
                         ViewBag.ThongBao = "Đăng ký thành công";
                     }
                     else
@@ -112,6 +125,124 @@ namespace Hasaki.Controllers
                 }
             }
             return RedirectToAction("Index","Home");
+        }
+        [HttpGet]
+        public ActionResult ForgotPass()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ForgotPass(string email)
+        {
+            var db = new HasakiDatabaseEntities();
+            if (string.IsNullOrEmpty(email))
+                ModelState.AddModelError(string.Empty, "Vui lòng nhập email");
+            if (ModelState.IsValid)
+            {
+                var khach = db.KhachHangs.FirstOrDefault(k => k.Email == email);
+                if (khach != null)
+                {
+                    // Gửi email đăng ký thành công
+                    SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                    client.EnableSsl = true;
+                    client.UseDefaultCredentials = false;
+                    client.Credentials = new NetworkCredential("lomki12231@gmail.com", "poka gibp icls gkvh");
+                    Random random = new Random();
+                    int rdn = random.Next(100000, 999999);
+                    Session["rdn"] = rdn;
+                    Session["email"] = email;
+                    MailMessage mailMessage = new MailMessage();
+                    mailMessage.From = new MailAddress("lomki12231@gmail.com");
+                    mailMessage.To.Add(email);
+                    //mailMessage.Body = $"";
+                    string url = Url.Action("SetPass", "LoginResgis", new { rdn = rdn }, protocol: Request.Url.Scheme);
+                    mailMessage.Body = $"Mã xác nhận của bạn là:  {rdn} \nHoặc click vào link sau:  {url}";
+                    mailMessage.Subject = "Verify code Hasaki";
+                    client.Send(mailMessage);
+                }
+                else
+                {
+                    ViewBag.ThongBao = "Không tồn tại tài khoản";
+                }
+                return RedirectToAction("ChekCode", "LoginResgis");
+            }
+            return RedirectToAction("ChekCode", "LoginResgis");
+        }
+        [HttpGet]
+        public ActionResult ChekCode()
+        {
+            if (Session["email"] != null)
+            {
+                return View();
+            }
+            return RedirectToAction("ForgotPass","LoginResgis");
+
+        }
+        [HttpPost]
+        public ActionResult ChekCode(int code)
+        {
+            if (Session["email"] != null)
+            {
+                int rdn = int.Parse(Session["rdn"].ToString());
+                if (code != rdn)
+                {
+                    ViewBag.ThongBao = "Sai mã";
+                    return View();
+                }
+                Session["check"] = 1;
+                return RedirectToAction("SetPass", "LoginResgis");
+            }
+            Session["check"] = null;
+            return RedirectToAction("ForgotPass", "LoginResgis");
+        }
+
+        [HttpGet]
+        public ActionResult SetPass(int? rdn)
+        {
+            if (rdn != null || (Session["email"] != null && Session["check"] != null))
+            {
+                return View();
+            }
+            return RedirectToAction("ForgotPass", "LoginResgis");
+        }
+
+        [HttpPost]
+        public ActionResult SetPass(string matkhau1, string matkhau2, int? rdn)
+        {
+            if (rdn != null || (Session["email"] != null && Session["check"] != null))
+            {
+                if (matkhau1 != matkhau2)
+                {
+                    ModelState.AddModelError(string.Empty, "Mật khẩu không hợp lệ");
+                }
+                else
+                {
+                    var db = new HasakiDatabaseEntities();
+                    string email = Session["email"] as string;
+                    var sha256 = SHA256.Create();
+                    byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(matkhau1));
+
+                    var sb = new StringBuilder();
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        sb.Append(bytes[i].ToString("x2"));
+                    }
+                    string matkhau = sb.ToString();
+                    var khach = db.KhachHangs.FirstOrDefault(k => k.Email == email);
+                    khach.MatKhau = matkhau;
+                    db.KhachHangs.AddOrUpdate(khach);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Finish","LoginResgis");
+            }
+            return RedirectToAction("Forgot","LoginResgis");
+        }
+
+        public ActionResult Finish() { return View(); }
+
+        public ActionResult UserPatial()
+        {
+            return PartialView();
         }
     }
 }
